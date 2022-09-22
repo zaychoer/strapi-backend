067f100b2a81b3a971098f3e2a3f9550d704d64e
zayyanadtn <100687520+zayyanadtn@users.noreply.github.com>
2022-09-22 15:56:40 +0700

    strapi backend postgre & s3

---
+++ strapi-backend/config/middlewares.js
@@ -0,0 +1,37 @@
+module.exports = [
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "seamless-aws-s3-images-bucket.s3.ap-southeast-1.amazonaws.com",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "seamless-aws-s3-images-bucket.s3.ap-southeast-1.amazonaws.com",
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+];
